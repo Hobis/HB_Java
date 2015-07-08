742028d600deea258600acf5635127534914c237
HobisJung <hobisjung@gmail.com>
2015-07-09 03:06:45 +0900

    2015-07-09

---
+++ 707_IntelliJ/Java_Core/HBLibs/src/hb/tests/core/ICallUser.java
@@ -0,0 +1,13 @@
+package hb.tests.core;
+
+/**
+ * Created by Hobis-PC on 2015-07-09.
+ */
+@SuppressWarnings({"unused", "UnnecessaryInterfaceModifier"})
+public interface ICallUser {
+
+    public String get_id();
+
+    public String get_name();
+
+}
